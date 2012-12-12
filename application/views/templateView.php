27c37c3b655def93c367e561dab34a7b9921c9b3
medvezatnik <medved2037@yandex.ru>
2012-12-12 18:12:52 +0400

    Fixing commit

--- application/views/templateView.php
+++
@@ -1,6 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<h1>Заголовок </h1>
-<?php include $contentView; ?>
-<h4>Футер<h4>
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
-								
